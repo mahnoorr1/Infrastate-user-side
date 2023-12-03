156705f8f2d88678a5eb285d80331677bd5896ed
hamza <hs623965@gmail.com>
2023-12-03 19:15:17 +0500

    SCD connected

--- src/Firebase/firebaseStorage.jsx
+++ src/Firebase/firebaseStorage.jsx
@@ -1,6 +1,8 @@
 import { ref, getDownloadURL, listAll, list } from 'firebase/storage';
 import storage from './firebaseConfig';
 
+
+
 // get single image
 export const getSingleImage = async (imagePath) => {
   try {
@@ -16,18 +18,33 @@ export const getSingleImage = async (imagePath) => {
 
 
 export const downloadTiffFile = async (zone, fileName) => {
-  const filePath = `${zone}/${fileName}.tif`;
+  const filePath = `Zone${zone}/z${zone}_${fileName}.tif`;
 
+  console.log(filePath)
   try {
     const fileRef = ref(storage, filePath);
     const downloadUrl = await getDownloadURL(fileRef);
+    const response = await fetch(downloadUrl);
 
-    return downloadUrl;
+    if (!response.ok) {
+      throw new Error(`Failed to fetch tif file: ${response.statusText}`);
+    }
+
+    const fileBlob = await response.blob();
+    const tifFile = new File([fileBlob], `z${zone}_${fileName}.tif`, {
+      type: 'image/tiff',
+      lastModified: new Date().getTime(), 
+    });
+
+    console.log("tif File from Firebase:", tifFile);
+    return tifFile; 
+    //return downloadUrl
   } catch (error) {
-    console.error(`Error downloading ${fileName}.tif from ${zone}:`, error);
+    console.error(`Error downloading ${fileName}.tif from Zone ${zone}:`, error);
     throw error;
   }
 };
+
 
 // all images 
 
@@ -45,6 +62,7 @@ export const getOuputImages = async (folderPath) => {
             .filter((item) => item.name.endsWith('.jpg'))
             .map(async (item) => {
               const imageUrl = await getDownloadURL(item);
+              console.log(imageUrl)
               return { imageUrl, fileName: item.name };
             })
         );
